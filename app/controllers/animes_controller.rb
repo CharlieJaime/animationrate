564ce9653e8bece5c972589df5ce91b3b7aceb06
Charlie Jaime <baroonie10@gmail.com>
2016-10-27 01:54:51 -0500

    new and create actions with views

--- app/controllers/animes_controller.rb
+++ app/controllers/animes_controller.rb
@@ -1,7 +1,18 @@
 class AnimesController < ApplicationController
 
   def index
-
   end
 
+  def new
+    @anime = Anime.new
+  end
+
+  def create
+    @anime = Anime.new(anime_params)
+  end
+
+  private
+  def anime_params
+    params.require(:anime).permit(:title, :description, :creator)
+  end
 end
