ec70a2ec3c78b78bb6cf54b5feff3ef1a565c404
Charlie Jaime <baroonie10@gmail.com>
2016-11-29 19:17:10 -0800

    create and show actions with views

--- app/controllers/animes_controller.rb
+++ app/controllers/animes_controller.rb
@@ -1,6 +1,11 @@
 class AnimesController < ApplicationController
+  before_action :find_anime, only: [:show, :edit, :update, :destroy]
 
   def index
+    @animes = Anime.all.order("created_at DESC");
+  end
+
+  def show
   end
 
   def new
@@ -9,10 +14,20 @@ class AnimesController < ApplicationController
 
   def create
     @anime = Anime.new(anime_params)
+
+    if @anime.save
+      redirect_to root_path
+    else
+      render "new"
+    end
   end
 
   private
   def anime_params
     params.require(:anime).permit(:title, :description, :creator)
+  end
+
+  def find_anime
+    @anime = Anime.find(params[:id]);
   end
 end
