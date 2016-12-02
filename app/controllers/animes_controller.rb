acce887e5bb00ff41f330d426e6ac44fb49eca00
Charlie Jaime <baroonie10@gmail.com>
2016-12-02 04:41:45 -0800

    function category filtering feature change in index page and action

--- app/controllers/animes_controller.rb
+++ app/controllers/animes_controller.rb
@@ -2,7 +2,12 @@ class AnimesController < ApplicationController
   before_action :find_anime, only: [:show, :edit, :update, :destroy]
 
   def index
-    @animes = Anime.all.order("created_at DESC");
+    if params[:category].blank?
+      @animes = Anime.all.order("created_at DESC");
+    else
+      @category_id = Category.find_by(name: params[:category]).id
+      @animes = Anime.where(:category_id => @category_id).order("created_at DESC")
+    end
   end
 
   def show
